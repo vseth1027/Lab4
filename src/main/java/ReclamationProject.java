73c8d2ee569106247a05614013074211261be179
vidushiseth <vidushiseth@wirelessprv-10-193-73-5.near.illinois.edu>
2017-09-26 14:15:31 -0500

    Fixed checkstyle errors in ReclamationProject

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -1,26 +1,40 @@
 /*
- * The following code needs a lot of TLC. So give it some!
- *
- * 1. Fix all checkstyle errors
- * 2. Determine what it does (it's going to be tough before you finish #1)
- * 3. Improve the name of the methods and variables
- * 4. Add comments and Javadoc comments where needed
- * 5. Remove unnecessary comments as appropriate
+ * The following code needs a lot of TLC. So give it some! 1. Fix all checkstyle errors 2. Determine
+ * what it does (it's going to be tough before you finish #1) 3. Improve the name of the methods and
+ * variables 4. Add comments and Javadoc comments where needed 5. Remove unnecessary comments as
+ * appropriate
  */
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
-        /*
-         * For loop with i
-         */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+/**
+ * Fix errors for this code in order to make it clean.
+ */
+public class ReclamationProject {
+    /**
+     * Finds the longest common substring for the entered string values.
+     * @param string1 is a String value.
+     * @param string2 is a String value.
+     * @return a String.
+     */
+    public static String findLongestCommonSubstring(final String string1, final String string2) {
+        String a = string1;
+        String b = string2;
+
+        if (a.length() > b.length()) {
+            String c = a;
+            a = b;
+            b = c;
+        }
+        String r = "";
+
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = a.length() - i; j > 0; j--) {
+                for (int k = 0; k < b.length() - j; k++) {
+                   if (a.regionMatches(i, b, k, j) && j > r.length()) {
+                       r =  a.substring(i, i + j);
+                   }
+                }
+            }
+        }
+        return r;
+    }
 }
